4de6edc83ed6e56bdb5c63722b0bb8d2d381d239
Obatola Opeyemi <opeyemiobatola@gmail.com>
2024-02-11 19:14:30 +0100

    refactored the code

--- index.js
+++ index.js
@@ -13,66 +13,66 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
+function getInputType(value) {
+  let inputType = "";
+
+  if (/\.[a-zA-Z]+$/.test(value)) {
+    inputType = "domain";
+  } else if (/^(\d{1,3}\.){3}\d{1,3}$/.test(value)) {
+    inputType = "ipAddress";
+  } else {
+    inputType = "";
+  }
+  return inputType;
+}
+
+function getUrl(value, dataType) {
+  let url = "";
+
+  if (dataType === "domain") {
+    url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_iGqIyna2yEvNqjgB78hFDglPpqBrW&${dataType}=${value}`;
+  } else if (dataType === "ipAddress") {
+    url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_iGqIyna2yEvNqjgB78hFDglPpqBrW&${dataType}=${value}`;
+  }
+  return url;
+}
+
+const fetchData = async (link) => {
+  const response = await fetch(link);
+  const data = await response.json();
+  return data;
+};
+
+function displayData(data) {
+  ipAddress.textContent = data.ip;
+  city.textContent = `${data.location["city"]}, ${data.location["region"]}`;
+  utc.textContent = data.location["timezone"];
+  isp.textContent = data.isp;
+  map.setView([data.location["lat"], data.location["lng"]], 13);
+
+  var marker = L.marker([data.location["lat"], data.location["lng"]]).addTo(
+    map
+  );
+  var circle = L.circle([data.location["lat"], data.location["lng"]], {
+    color: "red",
+    fillColor: "#f03",
+    fillOpacity: 0.5,
+    radius: 500,
+  }).addTo(map);
+}
+
+function displayFetchError() {
+  return alert("Type in a valid domain or IP address");
+}
+
 function getDetails(e) {
   e.preventDefault();
-
   const inputValue = inputField.value;
+  const dataType = getInputType(inputValue);
+  const link = getUrl(inputValue, dataType);
 
-  function getInputType() {
-    let inputType = "";
-
-    if (/\.[a-zA-Z]+$/.test(inputValue)) {
-      inputType = "domain";
-    } else if (/^(\d{1,3}\.){3}\d{1,3}$/.test(inputValue)) {
-      inputType = "ipAddress";
-    } else {
-      inputType = "invalid input";
-    }
-    return inputType;
-  }
-
-  const dataType = getInputType();
-
-  function getUrl() {
-    let url = "";
-
-    if (dataType === "domain") {
-      url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_iGqIyna2yEvNqjgB78hFDglPpqBrW&${dataType}=${inputValue}`;
-    } else if (dataType === "ipAddress") {
-      url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_iGqIyna2yEvNqjgB78hFDglPpqBrW&${dataType}=${inputValue}`;
-    }
-    return url;
-  }
-
-  const link = getUrl();
-
-  const fetchData = async () => {
-    const response = await fetch(link);
-    const data = await response.json();
-    return data;
-  };
-
-  fetchData()
-    .then((data) => {
-      ipAddress.textContent = data.ip;
-      city.textContent = `${data.location["city"]}, ${data.location["region"]}`;
-      utc.textContent = data.location["timezone"];
-      isp.textContent = data.isp;
-      map.setView([data.location["lat"], data.location["lng"]], 13);
-
-      var marker = L.marker([data.location["lat"], data.location["lng"]]).addTo(
-        map
-      );
-      var circle = L.circle([data.location["lat"], data.location["lng"]], {
-        color: "red",
-        fillColor: "#f03",
-        fillOpacity: 0.5,
-        radius: 500,
-      }).addTo(map);
-
-      console.log(data.location["lng"]);
-    })
-    .catch(() => alert("Opps! an error ocurred, check the data supplied"));
+  fetchData(link).then(displayData).catch(displayFetchError);
+  inputField.value = "";
 }
 
 btn.addEventListener("click", getDetails);
